785b18ddd5b07e14168690b1c2d46af46ad45f89
Jeff Zellner <jeffz@eml.cc>
2016-07-07 17:33:48 -0600

    run as root until volumes are more sorted

--- images/cassandra/run.sh
+++ images/cassandra/run.sh
@@ -67,4 +67,4 @@ echo CASSANDRA_BROADCAST_ADDRESS ${POD_IP}
 echo CASSANDRA_BROADCAST_RPC_ADDRESS ${POD_IP}
 
 export CLASSPATH=/kubernetes-cassandra.jar
-dumb-init cassandra -f
+dumb-init cassandra -f -R
