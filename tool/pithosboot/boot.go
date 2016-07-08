3f8c71ba0331c8aca617f891d4cf65a628a7fc77
Jeff Zellner <jeffz@eml.cc>
2016-07-08 13:27:58 -0600

    break out common code into rigging.git

--- tool/pithosboot/boot.go
+++ tool/pithosboot/boot.go
@@ -1,56 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
-
 	log "github.com/Sirupsen/logrus"
+	"github.com/gravitational/rigging"
 	"github.com/gravitational/trace"
 )
 
-func kubeCommand(args ...string) *exec.Cmd {
-	return exec.Command("/usr/local/bin/kubectl", args...)
-}
-
 func bootCluster() error {
-	err := createConfigMap("cassandra-cfg")
+	log.Infof("creating ConfigMap/cassandra-cfg")
+	out, err := rigging.CreateConfigMap("cassandra-cfg", "/var/lib/gravity/resources/cassandra-cfg")
+	if err != nil {
+		log.Error(out)
+		return trace.Wrap(err)
+	}
+
+	log.Infof("creating ConfigMap/pithos-cfg")
+	out, err = rigging.CreateConfigMap("pithos-cfg", "/var/lib/gravity/resources/pithos-cfg")
+	if err != nil {
+		log.Error(out)
+		return trace.Wrap(err)
+	}
+
+	log.Infof("creating pithos services + daemonset")
+	out, err = rigging.CreateFromFile("/var/lib/gravity/resources/pithos.yaml")
+	log.Info(out)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
-	err = createConfigMap("pithos-cfg")
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	err = createResources()
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	return nil
-}
-
-func createConfigMap(name string) error {
-	log.Infof("creating ConfigMap/%s", name)
-	path := fmt.Sprintf("/var/lib/gravity/resources/%s", name)
-	cmd := kubeCommand("create", "configmap", name, "--from-file="+path)
-	out, err := cmd.CombinedOutput()
-	log.Infof("cmd output: %s", string(out))
-	if err != nil {
-		return trace.Wrap(err)
-	}
-	return nil
-}
-
-func createResources() error {
-	log.Infof("creating pithos resources")
-	path := "/var/lib/gravity/resources/pithos.yaml"
-	cmd := kubeCommand("create", "-f", path)
-	out, err := cmd.CombinedOutput()
-	log.Infof("cmd output: %s", string(out))
-	if err != nil {
-		return trace.Wrap(err)
-	}
 	return nil
 }
