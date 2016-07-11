189bf2a3d2be777dcf7a369165b1515cca6d9c12
Jeff Zellner <jeffz@eml.cc>
2016-07-11 13:15:22 -0600

    checkpoint secret generation

--- tool/pithosboot/boot.go
+++ tool/pithosboot/boot.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"text/template"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/gravitational/rigging"
 	"github.com/gravitational/trace"
 )
 
 func bootCluster() error {
-
 	log.Infof("creating ConfigMap/cassandra-cfg")
 	out, err := rigging.CreateConfigMap("cassandra-cfg", "/var/lib/gravity/resources/cassandra-cfg")
 	if err != nil {
@@ -15,8 +20,7 @@ func bootCluster() error {
 		return trace.Wrap(err)
 	}
 
-	log.Infof("creating ConfigMap/pithos-cfg")
-	out, err = rigging.CreateConfigMap("pithos-cfg", "/var/lib/gravity/resources/pithos-cfg")
+	err = createPithosConfig()
 	if err != nil {
 		log.Errorf("%s", out)
 		return trace.Wrap(err)
@@ -42,6 +46,71 @@ func bootCluster() error {
 		if err != nil {
 			return trace.Wrap(err)
 		}
+	}
+
+	return nil
+}
+
+type AccessKeyList struct {
+	Keys []AccessKey
+}
+
+type AccessKey struct {
+	Key    string
+	Master bool
+	Tenant string
+	Secret string
+}
+
+func createPithosConfig() error {
+	log.Infof("creating ConfigMap/pithos-cfg")
+
+	keys := AccessKeyList{
+		Keys: []AccessKey{
+			AccessKey{
+				Key:    "C28D2EE399E1A4CBC295D677",
+				Master: true,
+				Tenant: "ops@gravitational.io",
+				Secret: "4C22FFEA7A6C9F3AAFD223B30628F0EA1D50318C2F6F173F",
+			},
+			AccessKey{
+				Key:    "CE9ED4F33A988422E7FA5DF1",
+				Master: false,
+				Tenant: "pithos",
+				Secret: "1D8943238E6BC13E9030DF912FD047C4176C7758778E0E90",
+			},
+		},
+	}
+
+	templateFile := "/var/lib/gravity/resources/pithos-cfg/config.yaml.template"
+
+	configTemplate, err := template.ParseFiles(templateFile)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	dir, err := ioutil.TempDir("", "pithosboot")
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	defer os.RemoveAll(dir)
+
+	buffer := &bytes.Buffer{}
+	err = configTemplate.Execute(buffer, keys)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	config := filepath.Join(dir, "config.yaml")
+	if err := ioutil.WriteFile(config, buffer.Bytes(), 0666); err != nil {
+		return trace.Wrap(err)
+	}
+
+	out, err := rigging.CreateConfigMap("pithos-cfg", dir)
+	if err != nil {
+		log.Errorf("%s", out)
+		return trace.Wrap(err)
 	}
 
 	return nil
