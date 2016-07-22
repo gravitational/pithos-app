7227d018936106520720c9d3cb0bc644fbe2f57a
Jeff Zellner <jeffz@eml.cc>
2016-07-22 15:12:38 -0600

    initial cfssl stuff

--- images/bootstrap/bootstrap.sh
+++ images/bootstrap/bootstrap.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+mkdir -p /etc/ssl && cd /etc/ssl
+
+cfssl gencert -initca ca-csr.json|cfssljson -bare ca -
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server server-csr.json | cfssljson -bare server
+
 /pithosboot
