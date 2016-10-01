965d1172ed065305ac7a921b0089d87a501c1562
Jeff Zellner <jeffz@eml.cc>
2016-10-01 12:28:39 -0600

    tweak certs

--- images/bootstrap/bootstrap.sh
+++ images/bootstrap/bootstrap.sh
@@ -3,15 +3,31 @@
 cd /root/cfssl
 
 cfssl gencert -initca ca-csr.json|cfssljson -bare ca -
-cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server default-server-csr.json | cfssljson -bare default-server
 
 kubectl create secret generic cluster-ca \
    --from-file=ca.pem=ca.pem \
    --from-file=ca-key=ca-key.pem \
    --from-file=ca.csr=ca.csr
 
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json \
+	-profile=server default-server-csr.json | cfssljson -bare default-server
+cp default-server.pem default-server-with-chain.pem
+cat ca.pem >> default-server-with-chain.pem
+
 kubectl create secret generic cluster-default-ssl \
 	--from-file=default-server.pem=default-server.pem \
+	--from-file=default-server-with-chain.pem=default-server-with-chain.pem \
+	--from-file=default-server-key.pem=default-server-key.pem \
+	--from-file=default-server.csr=default-server.csr
+
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json \
+	-profile=server kube-system-server-csr.json | cfssljson -bare kube-system-server
+cp kube-system-server.pem kube-system-server-with-chain.pem
+cat ca.pem >> kube-system-server-with-chain.pem
+
+kubectl create secret generic cluster-default-ssl \
+	--from-file=default-server.pem=default-server.pem \
+	--from-file=kube-system-server-with-chain.pem=kube-system-server-with-chain.pem \
 	--from-file=default-server-key.pem=default-server-key.pem \
 	--from-file=default-server.csr=default-server.csr
 
