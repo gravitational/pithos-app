8da27c0589e0305f3b46a8e7c747b0749dd32db8
Jeff Zellner <jeffz@eml.cc>
2016-10-17 12:46:01 -0600

    set appropriate replication factor when initializing

--- images/cassandra/initialize.sh
+++ images/cassandra/initialize.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+NUM_NODES=$(kubectl get nodes --selector=pithos-role=node --no-headers | wc --lines)
+REPLICATION_FACTOR=1
+
+if [[ $NUM_NODES -gt 2 ]]; then
+	REPLICATION_FACTOR=3
+fi
+
+sed -i 's/REPLICATION_FACTOR/'"${REPLICATION_FACTOR}"'/g' /init.cql
 
 cqlsh -f /init.cql cassandra.default.svc
