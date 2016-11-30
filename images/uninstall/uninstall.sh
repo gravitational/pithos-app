50eba6944f010787495030813be7839061123acc
Sergei Antipov <greendayonfire@gmail.com>
2016-11-30 09:50:00 +0700

    do not delete cluster ssl stuff

--- images/uninstall/uninstall.sh
+++ images/uninstall/uninstall.sh
@@ -5,12 +5,3 @@ kubectl delete configmap cassandra-cfg pithos-cfg
 kubectl delete -f /var/lib/gravity/resources/cassandra.yaml
 kubectl delete -f /var/lib/gravity/resources/pithos-rc.yaml
 kubectl delete secret cassandra-ssl pithos-keys
-
-for sname in cluster-ca cluster-default-ssl cluster-kube-system-ssl
-do
-	if kubectl get secret/$sname ; then
-		kubectl delete secret $sname
-	else
-		echo "secret/$sname already deleted"
-	fi
-done
