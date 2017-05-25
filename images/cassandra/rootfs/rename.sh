12180219024feae4f818ada84897749b14e4310f
Jeff Zellner <jeffz@eml.cc>
2017-05-25 12:02:06 -0600

    no more rename needed (#52)

--- images/cassandra/rootfs/rename.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "renaming local database"
-cqlsh -f /rename.cql localhost
-
-echo "flushing node"
-nodetool flush system
--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -8,34 +8,10 @@ if [ $1 = "update" ]; then
     echo "Starting update, changeset: $RIG_CHANGESET"
     rig cs delete --force -c cs/$RIG_CHANGESET
 
-	echo "Ensuring cluster name"
-	kubectl apply -f /var/lib/gravity/resources/rename-cassandra.yaml
-
-	pods=$(kubectl get po -l app=pithos-rename -ojsonpath='{.items[*].metadata.name}' 2> /dev/null)
-	for pod in $pods; do
-		echo "Checking pod $pod"
-		restart_count=$(kubectl get po $pod -ojsonpath='{.status.containerStatuses[0].restartCount}' 2> /dev/null)
-		echo "Restart count for $pod is $restart_count"
-		while (( restart_count < 1 )); do
-			sleep 2
-			restart_count=$(kubectl get po $pod -ojsonpath='{.status.containerStatuses[0].restartCount}' 2> /dev/null)
-		done
-		exit_code=$(kubectl get po $pod -ojsonpath='{.status.containerStatuses[0].lastState.terminated.exitCode}' 2> /dev/null)
-		echo "Exit code for $pod is $exit_code"
-		if [ "$exit_code" != "0" ]; then
-			kubectl logs $pod
-			>&2 echo pod $pod failed with $exit_code
-			exit $exit_code
-		fi
-	done
-
-	echo "Delete rename ds"
-	kubectl delete daemonset/cassandra-rename
-
     echo "Creating or updating resources"
-	kubectl delete configmap/cassandra-cfg
-	kubectl create configmap cassandra-cfg --from-file=/var/lib/gravity/resources/cassandra-cfg
-	rig delete rc/pithos --force
+    kubectl delete configmap/cassandra-cfg
+    kubectl create configmap cassandra-cfg --from-file=/var/lib/gravity/resources/cassandra-cfg
+    rig delete rc/pithos --force
 
     rig upsert -f /var/lib/gravity/resources/cassandra.yaml --debug
     rig upsert -f /var/lib/gravity/resources/pithos.yaml --debug
