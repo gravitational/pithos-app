4187b5dc282112bb88bf35ebaf60fa1d602d17d4
Jeff Zellner <jeffz@eml.cc>
2017-05-25 16:10:43 -0600

    update config to not use localhost during update

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -9,6 +9,10 @@ if [ $1 = "update" ]; then
     rig cs delete --force -c cs/$RIG_CHANGESET
 
     echo "Creating or updating resources"
+    kubectl get configmap/pithos-cfg -o yaml > pithoscfg.yaml
+    sed -i 's/localhost/cassandra.default.svc.cluster.local/' pithoscfg.yaml
+    kubectl apply -f pithoscfg.yaml
+
     kubectl delete configmap/cassandra-cfg
     kubectl create configmap cassandra-cfg --from-file=/var/lib/gravity/resources/cassandra-cfg
     rig delete rc/pithos --force
@@ -23,15 +27,10 @@ elif [ $1 = "rollback" ]; then
     echo "Reverting changeset $RIG_CHANGESET"
     rig revert
 
-    kubectl get ds/cassandra -o yaml > /tmp/cassandra-ds.yaml
-    if ! grep -q 'CASSANDRA_CLUSTER_NAME' /tmp/cassandra-ds.yaml
-    then
-        echo "Set CASSANDRA_CLUSTER_NAME env variable"
-        sed -i '0,/env:/s//env:\n        - name: CASSANDRA_CLUSTER_NAME\n          value: Pithos Cluster/' /tmp/cassandra-ds.yaml
-        kubectl replace -f /tmp/cassandra-ds.yaml
-        # Hack, because of "https://github.com/kubernetes/kubernetes/issues/29199"
-        kubectl delete po -l 'pithos-role=cassandra'
-    fi
+    kubectl get configmap/pithos-cfg -o yaml > pithoscfg.yaml
+    sed -i 's/cassandra.default.svc.cluster.local/localhost/' pithoscfg.yaml
+    kubectl apply -f pithoscfg.yaml
+
 else
     echo "Missing argument, should be either 'update' or 'rollback'"
 fi
