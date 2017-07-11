a4ed145c60e14aeb052bb27f743ff4aab287664b
Jeff Zellner <jeffz@eml.cc>
2017-07-11 12:39:33 -0600

    don't use -seq

--- images/cassandra/rootfs/usr/sbin/start-utils.sh
+++ images/cassandra/rootfs/usr/sbin/start-utils.sh
@@ -4,7 +4,7 @@ if [ "x$CRON_SCHEDULE" = "x" ]; then
     CRON_SCHEDULE='0 0 * * *'
 fi
 
-echo "$CRON_SCHEDULE "'root nodetool -p 7199 -h localhost repair -seq && nodetool -p 7199 -h localhost compact >> /var/log/cron.log 2>&1' > /etc/cron.d/cassandra
+echo "$CRON_SCHEDULE "'root nodetool -p 7199 -h localhost repair && nodetool -p 7199 -h localhost compact >> /var/log/cron.log 2>&1' > /etc/cron.d/cassandra
 
 # start telegraf
 /usr/bin/telegraf --quiet --config /etc/telegraf/telegraf-status.conf 2>&1 &
