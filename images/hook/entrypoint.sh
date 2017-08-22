d5f75c8a6f79a3b163a9b5797b1da7cd563bd627
Sergei Antipov <greendayonfire@gmail.com>
2017-08-23 01:12:15 +0700

    Delete deployments instead of rc

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -15,7 +15,7 @@ if [ $1 = "update" ]; then
 
     kubectl delete configmap/cassandra-cfg
     kubectl create configmap cassandra-cfg --from-file=/var/lib/gravity/resources/cassandra-cfg
-    rig delete rc/pithos --force
+    rig delete deployments/pithos --force
 
     rig upsert -f /var/lib/gravity/resources/cassandra.yaml --debug
     rig upsert -f /var/lib/gravity/resources/pithos.yaml --debug
