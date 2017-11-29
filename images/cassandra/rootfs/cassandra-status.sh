cc8724ca3faf789f92393b9aaf3f742259c52e9e
Sergei Antipov <greendayonfire@gmail.com>
2017-11-29 14:56:11 +0700

    Do not resolve ip address of cassandra node

--- images/cassandra/rootfs/cassandra-status.sh
+++ images/cassandra/rootfs/cassandra-status.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/nodetool -p 7199 -h localhost status -r | tail -n +6 | head -n -1 | /usr/bin/gawk -f /cassandra-status.awk
+/usr/bin/nodetool -p 7199 -h localhost status | tail -n +6 | head -n -1 | /usr/bin/gawk -f /cassandra-status.awk
