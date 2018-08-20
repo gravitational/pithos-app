cbf21ed00d53fa3a462de0183c5ccff9e53e51d8
Sergei Antipov <greendayonfire@gmail.com>
2018-08-20 03:44:02 -0400

    Add retry function to pithos-initialize

--- tool/pithosboot/boot.go
+++ tool/pithosboot/boot.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"strings"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/gravitational/rigging"
 	"github.com/gravitational/trace"
+)
+
+const (
+	retryTimes  = 20
+	retryPeriod = 5 * time.Second
 )
 
 func bootCluster() error {
@@ -29,8 +35,9 @@ func bootCluster() error {
 	}
 
 	log.Infof("waiting for pithos to be initialized")
-	err = rigging.WaitForJobSuccess("pithos-initialize", 10*time.Minute)
-	if err != nil {
+	if err := retry(context.TODO(), retryTimes, retryPeriod, func() error {
+		return rigging.WaitForJobSuccess("pithos-initialize", 10*time.Minute)
+	}); err != nil {
 		return trace.Wrap(err)
 	}
 
@@ -41,4 +48,23 @@ func bootCluster() error {
 	}
 
 	return nil
+}
+
+// retry will retry function X times until period is reached
+func retry(ctx context.Context, times int, period time.Duration, fn func() error) error {
+	if times < 1 {
+		return nil
+	}
+	err := fn()
+	for i := 1; i < times && err != nil; i++ {
+		log.Infof("Attempt %v, result: %v, retry in %v.", i+1, trace.Wrap(err), period)
+		select {
+		case <-ctx.Done():
+			log.Infof("Context is closing, return")
+			return err
+		case <-time.After(period):
+		}
+		err = fn()
+	}
+	return err
 }
