5b19ac2d9caddce9b31341758573077dd71c2320
Sergei Antipov <greendayonfire@gmail.com>
2019-07-07 16:39:49 -0400

    Fix night repair

--- images/cassandra/rootfs/usr/sbin/start-utils.sh
+++ images/cassandra/rootfs/usr/sbin/start-utils.sh
@@ -4,7 +4,7 @@ if [ "x$CRON_SCHEDULE" = "x" ]; then
     CRON_SCHEDULE='0 0 * * *'
 fi
 
-echo "$CRON_SCHEDULE "'root sleep $[RANDOM%30]m && nodetool -p 7199 -h localhost repair >> /var/log/cron.log 2>&1' > /etc/cron.d/cassandra
+echo -e "PATH=$PATH\n$CRON_SCHEDULE root sleep $((RANDOM%30))m && nodetool -p 7199 -h localhost repair >> /var/log/cron.log 2>&1" > /etc/cron.d/cassandra
 
 # watch /var/log/cron.log restarting if necessary
 cron && tail -f /var/log/cron.log
