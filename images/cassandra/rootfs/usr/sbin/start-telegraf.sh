48d2bf65419154cd2e153219ae7fcda3265485ca
Sergei Antipov <greendayonfire@gmail.com>
2019-10-10 18:01:48 -0400

    Delete curl calls from telegraf entrypoint

--- images/cassandra/rootfs/usr/sbin/start-telegraf.sh
+++ images/cassandra/rootfs/usr/sbin/start-telegraf.sh
@@ -11,11 +11,5 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-# create telegraf user
-curl -XPOST "http://influxdb.monitoring.svc:8086/query?u=root&p=root" \
-         --data-urlencode "q=CREATE USER ${INFLUXDB_TELEGRAF_USERNAME} WITH PASSWORD '${INFLUXDB_TELEGRAF_PASSWORD}'"
-curl -XPOST "http://influxdb.monitoring.svc:8086/query?u=root&p=root" \
-         --data-urlencode "q=GRANT ALL on k8s to ${INFLUXDB_TELEGRAF_USERNAME}"
-
 # start telegraf
 /usr/bin/telegraf --config /etc/telegraf/telegraf.conf
