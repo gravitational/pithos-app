0d13b5a506c46e6863ce442e20499bf8f8f996fd
Emanuele 'Lele' Calo <lele@gravitational.com>
2020-10-12 18:23:12 +0200

    Added explainatory comment

--- images/cassandra/rootfs/initialize.sh
+++ images/cassandra/rootfs/initialize.sh
@@ -7,6 +7,8 @@ if [[ $NUM_NODES -gt 2 ]]; then
 	REPLICATION_FACTOR=3
 fi
 
+# this sed command needs to happen in two steps or `sed` will fail because
+# of the read-only root filesystem
 sed 's/REPLICATION_FACTOR/'"${REPLICATION_FACTOR}"'/g' /init.cql > /tmp/init.cql
 cat /tmp/init.cql > /init.cql # avoid issues with sed and writing in /
 
