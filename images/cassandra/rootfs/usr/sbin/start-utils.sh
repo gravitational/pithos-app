94a7ef3935a03a30a0cfe7dcb993666fdde2f518
Sergei Antipov <sergei@gravitational.com>
2020-11-26 13:40:34 +0700

    Do not use user name for cron schedule

--- images/cassandra/rootfs/usr/sbin/start-utils.sh
+++ images/cassandra/rootfs/usr/sbin/start-utils.sh
@@ -4,6 +4,6 @@ if [ "x$CRON_SCHEDULE" = "x" ]; then
     CRON_SCHEDULE='0 0 * * *'
 fi
 
-echo -e "$CRON_SCHEDULE cassandra sleep $((RANDOM%30))m && nodetool -p 7199 -h localhost repair" > /tmp/cassandra.cron
+echo -e "$CRON_SCHEDULE sleep $((RANDOM%30))m && nodetool -p 7199 -h localhost repair" > /tmp/cassandra.cron
 
 supercronic /tmp/cassandra.cron
